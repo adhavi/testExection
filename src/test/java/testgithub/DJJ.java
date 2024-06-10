db093d0c574bcffbbd350f71c426a0fd23981aad
adhavi <95349108+adhavi@users.noreply.github.com>
2024-06-10 11:01:38 +0530

    commit and push

---
+++ src/test/java/testgithub/DJJ.java
@@ -0,0 +1,14 @@
+package testgithub;
+
+import org.testng.annotations.Test;
+
+public class DJJ {
+	
+	
+	@Test
+	public void demogit() 
+	{
+		System.out.println("Hi thsi is commit messsge by adhadu");
+	}
+
+}
